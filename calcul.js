bda3b118f90b8cd87f6664b6f5a5ab1718d04c37
Hamza <hbr589261@gmail.com>
2024-05-27 11:21:01 +0100

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,33 @@
+function calculator(){
+    
+    let display = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clean = document.querySelector('.clean');
+    let cutt = document.querySelector('.cutt');
+    let equal = document.querySelector('.equal');
+
+    buttons.forEach(function(albtn){
+        albtn.addEventListener('click', function(a){
+            let number = a.target.dataset.num;
+            display.value += number;
+        })
+    });
+    clean.addEventListener('click', function(){
+        if(display.value === display.value){
+            display.value = '';
+        }
+    });
+    cutt.addEventListener('click', function(){
+        if(display.value ===display.value){
+            display.value = display.value.toString().slice(0,-1);
+        }
+
+    });
+    equal.addEventListener('click', function(){
+        if(display.value === display.value){
+            let result = eval(display.value);
+            display.value = result;
+        }
+    })
+}
+calculator()
